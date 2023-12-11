ea5b381cc5c0c723f16265261a78a9f25c2c7a48
feroz-hub <bashaferoz66@gmail.com>
2023-12-11 22:49:51 +0530

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Feroze Basha on 10/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello Feroz")
+            .font(.title)
+            .fontWeight(.semibold)
+            .foregroundColor(.purple)
+            .padding()
+            
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Feroze Basha on 10/12/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
